f8666a71dc6c0f6be137a4bbad0ed77e53fe171e
zz9tf <zhengzheng@brandeis.edu>
2023-04-14 17:41:22 -0400

    fixed multiple docker dev.sh

--- dev.sh
+++ dev.sh
@@ -12,15 +12,15 @@ if [[ $1 == "--help" || $1 == "-h" ]]; then
     echo "  -i, --install          Install packages for React and Express"
     echo ""
     echo "Service and package installation:"
-    echo "  django <package>       Install a Django package with pip and restart the Django container"
-    echo "  react <package>        Install an npm package for React"
+    echo "  django  <package>       Install a Django package with pip and restart the Django container"
+    echo "  react   <package>        Install an npm package for React"
     echo "  express <package>      Install an npm package for Express"
     echo ""
 else
     if [[ $1 == "django" ]]; then
         source activate hire_me_now
         pip install $2
-        docker-compose up -d django
+        STATUS=development docker-compose up -d django
 
     elif [[ $1 == "react" ]]; then
         cd react && npm install $2 --save
@@ -30,24 +30,23 @@ else
 
     elif [[ $1 == "--deploy" || $1 == "-d" ]]; then
         docker rm -vf $(docker ps -aq)
-        AGU1=deploy docker-compose up
+        STATUS=deploy docker-compose up
 
     elif [[ $1 == "--clear" || $1 == "-c" ]]; then
         docker rm -vf $(docker ps -aq)
-        docker rmi -f $(docker images -aq)
+        docker system prune -a -f
         git remote prune origin
 
     elif [[ $1 == "--rebuild" || $1 == "-r" ]]; then
         docker rm -vf $(docker ps -aq)
-        docker rmi -f $(docker images -aq)
-        docker-compose up
+        docker system prune -a -f
+        STATUS=development docker-compose up  --no-deps --build -V
 
     elif [[ $1 == "--install" || $1 == "-i" ]]; then
         cd react && npm install --save
         cd ../express && npm install --save
 
     else
-        docker-compose up
-
+        STATUS=development docker-compose up
     fi
 fi
