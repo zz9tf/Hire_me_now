693306c1b0be24bbd0187a734ceb3779e9c8226c
Zheng <zhengzheng@brandeis.edu>
2023-04-16 20:50:46 -0400

    update ec2

--- dev.sh
+++ dev.sh
@@ -43,18 +43,19 @@ elif [ -z "$1" ]; then
 else
     if [[ $1 != "--help" && $1 != "-h" ]]; then
         echo ""
-        echo "unknown shorthand flag: $1"
+        echo "Error: unknown command '$1'."
+        
     fi
     echo ""
     echo "Usage: ./dev.sh [OPTIONS] [SERVICE] [PACKAGE]"
     echo ""
-    echo "Options:"
+    echo "OPTIONS:"
+    echo "  -b, --build            Build the React app"
+    echo "  -c, --clear            Remove all Docker containers and images, and prune the system"
+    echo "  -d, --deploy           Deploy the app using Docker Compose and Let's Encrypt"
+    echo "  -i, --install          Install all dependencies for the React and Express apps"
+    echo "  -r, --rebuild          Rebuild all Docker containers from scratch"
     echo "  -h, --help             Show this help message"
-    echo "  -d, --deploy           Deploy services (You need to have build folder in react before deploy.)"
-    echo "                             ie: 'npm run build' in react to get the build folder"
-    echo "  -c, --clear            Remove all containers and images, and prune remote git branches"
-    echo "  -r, --rebuild          Rebuild all containers"
-    echo "  -i, --install          Install packages for React and Express"
     echo ""
     echo "Service and package installation:"
     echo "  django  <package>       Install a Django package with pip and restart the Django container"
